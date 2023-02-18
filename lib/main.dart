b314ba8dfc9ace256c75093782c3007e7b215184
Kairos Jeong <kairosys@gmail.com>
2023-02-18 02:43:02 -0800

    Dart Fetching -api service

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toonsflix/screens/home_screen.dart';
+import 'package:toonsflix/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const MyApp());
 }
 
